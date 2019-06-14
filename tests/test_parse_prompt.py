773f81e4d7bc8865f3988e08b6bce4db978d38c8
Dull Bananas <dull.bananas0@gmail.com>
2019-06-13 21:05:26 -0700

    Add test for platform option of PS1

--- tests/test_parse_prompt.py
+++ tests/test_parse_prompt.py
@@ -3,3 +3,7 @@ from shellp.parse_prompt import parse_prompt
 
 def test_prompt_symbol():
 	assert parse_prompt('{symbol}') in ('$', '#')
+
+def test_prompt_platform():
+	import platform
+	assert parse_prompt('{plaform["processor"]}') == platform.processor()
