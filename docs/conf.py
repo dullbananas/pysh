c0ff1954d332f8ccfcbd4e947d6fc91639b5f72c
Dull Bananas <dull.bananas0@gmail.com>
2019-06-30 19:28:05 -0700

    Set Alabaster theme options

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,12 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+	'github_button': True,
+	'github_repo': 'dullbananas',
+	'github_user': 'shellp',
+	'show_relbars': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
