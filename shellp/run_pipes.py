acd33b758eb4cb26f0a14b6071b343bfe8186fbd
Dull Bananas <dull.bananas0@gmail.com>
2019-07-10 16:45:55 -0700

    Fix some issues with pipes

--- shellp/run_pipes.py
+++ shellp/run_pipes.py
@@ -1,5 +1,6 @@
 import subprocess
-from . import utils
+import time
+import signal
 
 
 def run_pipeline(cmds):
@@ -16,6 +17,7 @@ def run_pipeline(cmds):
 	
 	# Create first process
 	procs.append(subprocess.Popen(cmds[0], stdout=subprocess.PIPE))
+	time.sleep(0.1)
 	
 	# Create processes in the middle
 	for index, cmd in enumerate(cmds):
@@ -25,7 +27,10 @@ def run_pipeline(cmds):
 			procs.append(subprocess.Popen(cmd, stdin=procs[index-1].stdout, stdout=subprocess.PIPE))
 		else:
 			procs.append(subprocess.Popen(cmd, stdin=procs[index-1].stdout))
+		time.sleep(0.1)
 	
 	# Communicate with processes
+	old_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
 	procs[0].communicate()
+	signal.signal(signal.SIGINT, old_handler)
 	procs[-1].wait()
