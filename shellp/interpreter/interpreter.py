1153fac640333952974cff4478a4090010430533
Dull Bananas <dull.bananas0@gmail.com>
2019-08-16 14:26:48 -0700

    Create interpreter
    Currently it only can run commands

--- shellp/interpreter/interpreter.py
+++ shellp/interpreter/interpreter.py
@@ -1,8 +1,62 @@
+import re
+import os
+
+
+
+patterns = {
+}
+
+
 class Line:
-	def __init__(self, text):
-		self.type
+	@staticmethod
+	def factory(line):
+		# Get indent_level and text
+		indent_level = None
+		for i, char in enumerate(line):
+			if char != '\t':
+				indent_level = i
+				break
+		if indent_level == None:
+			return None
+		text = line[indent_level:]
+		
+		# Get type of line and return instance of the corresponding class
+		return Command(indent_level=indent_level, text=text)
+
+
+
+class Command(Line):
+	def __init__(self, indent_level, text):
+		self.indent_level = indent_level
+		self.text = text
+	
+	
+	def run(self):
+		os.system(self.text)
+
 
 
 class Interpreter:
 	def __init__(self):
 		self.lines = []
+	
+	
+	def feed_line(self, line):
+		line = Line.factory(line)
+		if line == None:
+			return False
+		self.lines.append(line)
+		if self.ready():
+			self.execute()
+			self.lines = []
+			return False
+		else:
+			return True
+	
+	
+	def ready(self):
+		return True
+	
+	
+	def execute(self):
+		self.lines[0].run()
--- shellp/main.py
+++ shellp/main.py
@@ -1,2 +1,29 @@
-def main(**kwargs):
-	print(kwargs)
+from .__init__ import __version__
+from .interpreter import Interpreter
+import sys
+
+
+def main(interactive, use_user_config):
+	if interactive:
+		print(f'ShellP version {__version__}')
+	
+	interpreter = Interpreter()
+	
+	if not interactive:
+		while True:
+			line = sys.stdin.readline()
+			if line == '':
+				return 0
+			else:
+				interpreter.feed_line(line[:-1])
+	
+	while True:
+		try:
+			if interactive:
+				prompt = '$ '
+			else:
+				prompt = ''
+			cmd = input(prompt)
+			interpreter.feed_line(cmd)
+		except EOFError:
+			sys.exit(0)
